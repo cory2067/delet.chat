e63d924158e11d32bc7bb7bbfa05d344011ab7b8
Cory Lynch <cjl2625@gmail.com>
2016-12-22 22:35:03 -0500

    uploaded nonsense basic server

---
+++ server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit, join_room, leave_room
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@socketio.on('msg')
+def handle_msg(msg):
+	print('received' + msg)
+	emit('receive', 'meme', broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', room=room)
+
+
