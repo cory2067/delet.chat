ed896671926bacc524086c7f884a27ae39ce9982
EC2 Default User <ec2-user@ip-172-31-22-112.us-west-2.compute.internal>
2016-12-23 21:02:49 +0000

    fixed server hanging issue

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
+import eventlet
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -12,21 +13,22 @@ def index():
 
 @socketio.on('msg')
 def handle_msg(msg):
-	#Messages are in the form ROOM:::MESSAGE
-	print('received ' + msg)
-	emit('receive', msg.split(":::")[1], room=msg.split(":::")[0])
+        #Messages are in the form ROOM:::MESSAGE
+        print('received ' + msg)
+        emit('receive', msg.split(":::")[1], room=msg.split(":::")[0])
+        eventlet.sleep(0)
 
 @socketio.on('join')
 def on_join(room):
-	global id
-	id+=1
-	join_room(room)
-	print(room)
-	#emit('confirm', str(id))
-	emit('receive', 'User '+str(id)+' has entered the room.', room=room)
+        global id
+        id+=1
+        join_room(room)
+        print(room)
+        #emit('confirm', str(id))
+        emit('receive', 'User '+str(id)+' has entered the room.', room=room)
+        eventlet.sleep(0)        
 
-
-@socketio.on('leave')
-def on_leave(room):
-	leave_room(room)
-	send('memer has left the room.', room=room)
+@socketio.on('disconnect')
+def on_leave():
+        print("somebody disconnected")
+socketio.run(app, host="0.0.0.0", port=80)
