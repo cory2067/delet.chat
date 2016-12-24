4157c222c1def8a5f56f1c62b621b2c4d01dab3a
seanhackett55 <seanhackett55@gmail.com>
2016-12-24 14:58:55 -0500

    worked on room creation + room name management

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
 import eventlet
 
+import sqlite3
+conn = sqlite3.connect('chatRoom.db')
+c = conn.cursor()
+c.execute("CREATE TABLE ChatRooms (name TEXT);")
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -11,6 +16,23 @@ id=0
 def index():
 	return render_template('index.html')
 
+@app.route('/a/homepage')
+def homepage():
+	return render_template('homepage.html')
+
+@app.route('/<roomID>/')
+def room(roomID):
+	return render_template('index.html',room=roomID)
+
+@socketio.on('createRoom')
+def create_room():
+	while (true):
+		#generate chat room name
+		roomName = '23q4adsf';
+		if (c.execute("SELECT TOP 1 name FROM ChatRooms WHERE name=\""+roomID+"\"" == ""):
+		     return "//"+roomID+"//"
+	return null
+	
 @socketio.on('msg')
 def handle_msg(msg):
         #Messages are in the form ROOM:::MESSAGE
