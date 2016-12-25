b97ab5cc67ae63640a8e6fa8498f1dfc838604fc
seanhackett55 <seanhackett55@gmail.com>
2016-12-25 01:33:18 -0500

    Update server.py

--- server.py
+++ server.py
@@ -22,25 +22,26 @@ def index():
 def homepage():
 	return render_template('homepage.html')
 
-@app.route('/chat/<roomID>/')
+@app.route('/a/chat/<roomID>/')
 def room(roomID):
-	return render_template('index.html',room=roomID)
+	return render_template('chatroom.html',room=roomID)
 
 #homepage sends a POST request, returns a new room
 @app.route('/create', methods = ['POST'])
 def create_room():
 	#list of all alphanumeric characters
-	chars = [chr(a) for a in (range(48,58)+range(97,123)+range(65,91))]
+	chars = [chr(a) for a in (list(range(48,58))+list(range(97,123))+list(range(65,91)))]
 	while True:
 		#generate chat room name
 		roomID = "".join([choice(chars) for a in range(8)])
 		c.execute("SELECT * FROM ChatRooms WHERE name=\""+roomID+"\"")
-		print("yo boi:"+c.fetchall())
-		return "lmaoimaurl.com"
-		'''if c.fetchall():
+		print("yo boi:"+"".join(c.fetchall()))
+		print(roomID)
+		if "".join(c.fetchall()) == "":
 			c.execute("INSERT INTO rooms VALUES (\""+roomID+"\")")
 			conn.commit()
-			return roomID'''
+			return roomID
+
 
 @socketio.on('msg')
 def handle_msg(msg):
@@ -63,3 +64,4 @@ def on_join(room):
 def on_leave():
         print("somebody disconnected")
 socketio.run(app, host="0.0.0.0", port=80)
+#socketio.run(app, host="127.0.0.1", port=5000) for testing
